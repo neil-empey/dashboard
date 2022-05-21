68b8f14e882e20cf5aef87a8879f856620229b29
Neil Empey <argus.two.2@gmail.com>
2022-05-21 09:50:25 -0600

    added room routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/devices/:id', to: 'devices#update'
   get '/device/1', to: 'devices#frontdoor'
   get '/frontdoor', to: 'devices#frontMotion'
+  get '/device/2', to: 'devices#room'
+  get '/room', to: 'devices#roomMotion'
 
 end
